0aedd85aaedd430143e8f735a83a2d901bcded5e
bmmukhridin <73002669+bmmukhridin@users.noreply.github.com>
2022-08-06 00:18:17 -0700

    javascript

---
+++ Quote-Generator/script.js
@@ -0,0 +1,10 @@
+// Get Quotes From API
+
+async function getQuotes() {
+  const apiUrl = "https://type.fit/api/quotes";
+  try {
+    const response = await fetch(apiUrl);
+  } catch (error) {
+    alert(error);
+  }
+}
