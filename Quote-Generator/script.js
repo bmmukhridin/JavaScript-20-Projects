29961a804d7ca173d2ab135b4aa7f1eca5e9c025
bmmukhridin <73002669+bmmukhridin@users.noreply.github.com>
2022-08-07 14:08:18 -0700

    added

--- Quote-Generator/script.js
+++ Quote-Generator/script.js
@@ -1,17 +1,27 @@
-const quoteContainer = document.getElementById("quote-container")
-const quoteText = document.getElementById("quote")
-const authorText = document.getElementById("author")
-const twitterBtn = document.getElementById("twitter")
-const newQuoteBtn = document.getElementById("new-quote")
+const quoteContainer = document.getElementById("quote-container");
+const quoteText = document.getElementById("quote");
+const authorText = document.getElementById("author");
+const twitterBtn = document.getElementById("twitter");
+const newQuoteBtn = document.getElementById("new-quote");
 
 let apiQuotes = [];
 // Get New Quotes
 
 function newQuotes() {
   const getNewQuotes = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-  authorText.textContent=getNewQuotes.author;
-  quoteText.textContent=getNewQuotes.text
-//   console.log(getNewQuotes)
+  //   Check author Name if No name put "Unkown"
+  if (getNewQuotes.author == null) {
+    authorText.textContent = "Unknown";
+  } else {
+    authorText.textContent = getNewQuotes.author;
+  }
+//   check qutoe length for style
+if(getNewQuotes.text.length){
+    quoteText.classList.add("long-quote")
+}else{
+    quoteText.classList.remove("long-quote")
+}
+  quoteText.textContent = getNewQuotes.text;
 }
 
 // Get Quotes From API
@@ -25,5 +35,14 @@ async function getQuotes() {
     alert(error);
   }
 }
+// Tweet Quote
+function tweetQuote(){
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+    window.open(twitterUrl, '_blank')
+}
+
+
+newQuoteBtn.addEventListener("click", newQuotes);
+twitterBtn.addEventListener("click", tweetQuote)
 // onLoad
 getQuotes();
