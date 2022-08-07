443fb0edc6abd6551b3aba2adc874c522b0b2f52
bmmukhridin <73002669+bmmukhridin@users.noreply.github.com>
2022-08-07 13:35:47 -0700

    new-javascript

--- Quote-Generator/script.js
+++ Quote-Generator/script.js
@@ -1,8 +1,17 @@
+const quoteContainer = document.getElementById("quote-container")
+const quoteText = document.getElementById("quote")
+const authorText = document.getElementById("author")
+const twitterBtn = document.getElementById("twitter")
+const newQuoteBtn = document.getElementById("new-quote")
+
 let apiQuotes = [];
 // Get New Quotes
 
 function newQuotes() {
   const getNewQuotes = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+  authorText.textContent=getNewQuotes.author;
+  quoteText.textContent=getNewQuotes.text
+//   console.log(getNewQuotes)
 }
 
 // Get Quotes From API
@@ -15,7 +24,7 @@ async function getQuotes() {
   } catch (error) {
     alert(error);
   }
+  
 }
 // onLoad
-
 getQuotes();
