9cd25f84a179317fb71d8b00a3539767677c5854
bmmukhridin <73002669+bmmukhridin@users.noreply.github.com>
2022-08-11 23:22:40 -0700

    added-api

---
+++ Infite-Scroll/script.js
@@ -0,0 +1,19 @@
+// Unsplash API
+const count = 10;
+const apiKEY = 'cxTwpGnnBqXXNwqOistwB2A89HKZolvcfH1JDrc510M';
+const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKEY}&count`
+// Get photo from API
+
+async function getPhoto() {
+    try {
+        const response = await fetch(apiUrl)
+        const data = await response.json()
+        console.log(response)
+        }catch(error){
+            alert(error)
+        }
+}
+
+// On Load 
+getPhoto()
+console.log("hello")
