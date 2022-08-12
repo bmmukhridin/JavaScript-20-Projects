964e75168f25ab2bbb30bbf37739b4afb65afb37
bmmukhridin <73002669+bmmukhridin@users.noreply.github.com>
2022-08-12 09:38:14 -0700

    fixed-api

--- Infite-Scroll/script.js
+++ Infite-Scroll/script.js
@@ -1,14 +1,27 @@
+const imageContainer = document.getElementById("image-container");
+const loader = document.getElementById("loader")
+
+let photosArray = []
+
 // Unsplash API
 const count = 10;
 const apiKEY = 'cxTwpGnnBqXXNwqOistwB2A89HKZolvcfH1JDrc510M';
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKEY}&count=${count}`
 // Get photo from API
 
+function displayPhotos() {
+    photosArray.forEach((photo)=>{
+        console.log(photo.links.html)
+    })
+}
+
+
 async function getPhoto() {
     try {
         const response = await fetch(apiUrl)
-        const data = await response.json()
-        console.log(response)
+        photosArray = await response.json()
+        displayPhotos()
+        //console.log(photosArray)
         }catch(error){
             alert(error)
         }
